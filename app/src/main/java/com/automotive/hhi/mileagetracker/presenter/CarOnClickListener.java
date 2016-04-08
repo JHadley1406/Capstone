5c2bbc2e324ed230db04a0450c91f880ce129cfb
Josiah <josiah.hadley@gmail.com>
2016-04-08 10:51:59 -0700

    fix: Missed CarOnClickListener on last commit, adding now

---
+++ app/src/main/java/com/automotive/hhi/mileagetracker/presenter/CarOnClickListener.java
@@ -0,0 +1,12 @@
+package com.automotive.hhi.mileagetracker.presenter;
+
+import com.automotive.hhi.mileagetracker.model.data.Car;
+
+/**
+ * Created by josiahhadley on 4/7/16.
+ */
+public interface CarOnClickListener {
+
+    void onClick(Car car);
+
+}
